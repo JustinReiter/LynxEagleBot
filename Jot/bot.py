ea9cc952448a24ca6c0fb50437a5191c37eff230
Justin Reiter <JDReiter@uwaterloo.ca>
2021-04-26 01:07:56 +0100

    Finish bot for Joi

--- Jot/bot.py
+++ Jot/bot.py
@@ -2,6 +2,8 @@ import os
 import discord
 from dotenv import load_dotenv
 from datetime import datetime
+from discord.utils import get
+import random
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -25,9 +27,9 @@ Civ 6: :regional_indicator_c:
 Catan: :farmer:
 CSGO: :gun:
 L4D2: :zombie:
-Politics: :judge:
+Politics: :office_worker:
 Rocket League: :red_car:"""
-emojis = []
+emojis = [":x:", ":regional_indicator_w:", ":chess_pawn:", ":regional_indicator_c:", ":farmer:", ":gun:", ":zombie:", ":office_worker:", ":red_car:"]
 
 emote_to_role = {
     ":x:": "Picking Competition",
@@ -37,15 +39,30 @@ emote_to_role = {
     ":farmer:": "Catan",
     ":gun:": "CS:GO",
     ":zombie:": "L4D2",
-    ":judge:": "Politics",
-    ":red_car:": "Rocket League"
+    ":office_worker:": "Politics",
+    ":red_car:": "Rocket League",
+    ":judge:": "Judge",
+    ":student:": "contestant"
 }
+
+quotes = [
+    '"you cant be more motivated than my motivation to not be motivated"',
+    '"From a scale of \'Total Noob\' to \'Noob\' they are \'Free Win\'"',
+    '"our entire pick set was basicly counter them... we failed misserably getting 1-6"',
+    '"ah [he] is a nub \*hides the fakt i lost to him\*"',
+    '"every thing is going absolutly fine. i only kicked 2 people for no reason so far"'
+]
 
 def log_message(msg):
     time_str = "[" + datetime.now().isoformat() + "] Jot: "
+
+    fw = open("logs/" + str(datetime.now().date()).replace("-", ""), 'a')
+    fw.write("{}\n".format(time_str + msg))
+    fw.close()
     print(time_str + msg)
 
 async def send_init_message(message: discord.Message):
+    log_message("Sending init message for roles")
     sent_message = await message.channel.send(content=general_role_msg)
     for emoji in emojis:
         await sent_message.add_reaction(emoji)
@@ -55,43 +72,49 @@ async def on_message(message: discord.Message):
     if message.author.id == BOT_ID:
         # Don't process bot messages
         return
-    if message.author.id in admins and message.context == "j!init":
-        await message.channel.send(content=":tada: Woohoo, I saw what you said", tts=True)
-    elif message.author.id in admins and message.context.startswith("j!event"):
+    if message.author.id in admins and message.content == "j!init":
+        send_init_message(message)
+    elif message.author.id in admins and message.content.startswith("j!event"):
+        # New event message
+        log_message("Started new picking competition event on {}".format(message.content[8:]))
         sent_message = await message.channel.send("New picking competition on {}!\n\nIf you are interested in joining, use :judge: to be a judge and :student: to be a contestant.".format(message.context[8:]))
         await sent_message.add_reaction(":judge:")
         await sent_message.add_reaction(":student:")
+    elif message.content == "j!quote":
+        # Quotes easter egg
+        quote = random.choice(quotes)
+        await message.channel.send(quote)
+        log_message("{} requested quote {} in {}".format(message.author.name + message.author.discriminator, quotes.index(quote), message.channel.name))
+    elif message.content == "j!links":
+        # Links to youtube/twitch
+        await message.channel.send("Links:\nYoutube: <https://www.youtube.com/channel/UCzVdEwv0OYkXtoh5eOw0Tyw>\nTwitch: <https://www.twitch.tv/joir179>")
+        log_message("{} requested Joi links in {}".format(message.author.name + message.author.discriminator, message.channel.name))
     
 
 @client.event
-async def on_reaction_add(reaction: discord.Reaction, user: discord.User):
+async def on_reaction_add(reaction: discord.Reaction, user: discord.Member):
     if user.id == BOT_ID:
         # Don't process bot actions
         return
-    if reaction.message.channel.name == ROLES_CHANNEL:
-        await user.add_roles()
-        await reaction.message.channel.send("Found an emote added by {}".format(user.name))
-        print("msg: {}".format(reaction.message.content))
+    if reaction.message.channel.name == ROLES_CHANNEL and reaction.emoji.name in emote_to_role:
+        await user.add_roles(get(reaction.message.server.roles, name=emote_to_role[reaction.emoji.name]))
+        log_message("Added {} role for {}".format(emote_to_role[reaction.emoji.name], user.name + user.discriminator))
 
 @client.event
 async def on_reaction_remove(reaction: discord.Reaction, user: discord.Member):
     if user.id == BOT_ID:
         # Don't process bot actions
         return
-    if reaction.message.channel.name == ROLES_CHANNEL:
-        await user.remove_roles()
-        await reaction.message.channel.send("Found an emote removed by {}".format(user.name))
-        print("msg: {}".format(reaction.message.content))
+    if reaction.message.channel.name == ROLES_CHANNEL and reaction.emoji.name in emote_to_role:
+        await user.remove_roles(get(reaction.message.server.roles, name=emote_to_role[reaction.emoji.name]))
+        log_message("Removed {} role from {}".format(emote_to_role[reaction.emoji.name], user.name + user.discriminator))
 
 @client.event
 async def on_ready():
-    print("Running from bot.py")
-    for guild in client.guilds:
-        print("[" + datetime.now().isoformat() + "] Jot: Connected")
-        if guild.id == GUILD_ID:
-            print(
-                f"{client.user} is connected to the following guild:\n"
-                f"{guild.name}(id: {guild.id})\n"
-            )
+    log_message("Bot is online")
+
+@client.event
+async def on_disconnect():
+    log_message("Bot has disconnected")
 
 client.run(TOKEN)
