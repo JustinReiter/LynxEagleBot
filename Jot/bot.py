0c9b90cfcbd9ced725ab3e118a150bdba6406be5
Justin Reiter <JDReiter@uwaterloo.ca>
2021-04-28 00:59:44 +0100

    Finalize jot the bot

--- Jot/bot.py
+++ Jot/bot.py
@@ -1,3 +1,5 @@
+
+
 import os
 import discord
 from dotenv import load_dotenv
@@ -8,7 +10,8 @@ import random
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = os.getenv("DISCORD_GUILD_ID")
-ROLES_CHANNEL = os.getenv("ROLES_CHANNEL")
+ROLES_CHANNEL = int(os.getenv("ROLES_CHANNEL"))
+COMPETITION_CHANNEL = int(os.getenv("COMPETITION_CHANNEL"))
 BOT_ID = int(os.getenv("BOT_ID"))
 
 admins = [
@@ -19,38 +22,50 @@ admins = [
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True)
 client = discord.Client(intents=intents)
 
-general_role_msg = """If you want a role, react any of these:
-Picking Competition: :x:
-Warzone: :regional_indicator_w:
-Chess: :chess_pawn:
-Civ 6: :regional_indicator_c:
-Catan: :farmer:
-CSGO: :gun:
-L4D2: :zombie:
-Politics: :office_worker:
-Rocket League: :red_car:"""
-emojis = ["❌", "🇼", "♟️", "🇨", "🧑‍🌾", "🔫", "🧟", "🧑‍💼", "🚗"]
+help_msg = """
+
+"""
+
+general_role_msg = """Basic Access
+:eight_spoked_asterisk: - Guest: Gives you basic access on the server
+
+Games Access
+:game_die: - Warzone: Access to all Warzone related channels
+:video_game: - Other Games: If you play chess, catan, civ 6, rocket league, l4d2 or cs:go you can react to this and give yourself acces to the Other Games channel
+
+Training Access
+:reminder_ribbon: - Warzone Training: Gives you access to all improvement oriented Channels for Warzone
+
+Events Access
+:medal: - Picking Competition: Gives you acces to all Picking Competition related Channels. Giving yourself this Role will show me your interest in Participating in this event and will be used to @ people.
+
+Languages
+:flag_nl: - Dutch
+:flag_fr: - French
+:flag_de: - German
+
+Other
+:office_worker: - Politics: Gives you acces to Political orientated Channels."""
+emojis = ["✳️", "🎲", "🎮", "🎗️", "🏅", "🇳🇱", "🇫🇷", "🇩🇪", "🧑‍💼"]
 
 emote_to_role = {
-    "❌": "Picking Competition",
-    "🇼": "Warzone",
-    "♟️": "Chess",
-    "🇨": "Civ 6",
-    "🧑‍🌾": "Catan",
-    "🔫": "CS:GO",
-    "🧟": "L4D2",
+    "🏅": "Picking Competition",
+    "🎲": "Warzone",
+    "🎮": "Other Games",
     "🧑‍💼": "Politics",
-    "🚗": "Rocket League",
-    "🧑‍⚖️": "Judge",
-    "🧑‍🎓": "Contestant"
+    "🇳🇱": "Dutch",
+    "🇫🇷": "French",
+    "🇩🇪": "German",
+    "🎗️": "Training",
+    "✳️": "Guest"
 }
 
 quotes = [
-    '"you cant be more motivated than my motivation to not be motivated"',
-    '"From a scale of \'Total Noob\' to \'Noob\' they are \'Free Win\'"',
-    '"our entire pick set was basicly counter them... we failed misserably getting 1-6"',
-    '"ah [he] is a nub \*hides the fakt i lost to him\*"',
-    '"every thing is going absolutly fine. i only kicked 2 people for no reason so far"'
+    '"you cant be more motivated than my motivation to not be motivated" -Joi',
+    '"From a scale of \'Total Noob\' to \'Noob\' they are \'Free Win\'" -Joi',
+    '"our entire pick set was basicly counter them... we failed misserably getting 1-6" -Joi',
+    '"ah [he] is a nub \*hides the fakt i lost to him\*" -Joi',
+    '"every thing is going absolutly fine. i only kicked 2 people for no reason so far" -Joi'
 ]
 
 def log_message(msg):
@@ -63,9 +78,9 @@ def log_message(msg):
 
 async def send_init_message(message: discord.Message):
     log_message("Sending init message for roles")
-    sent_message = await message.channel.send(content=general_role_msg)
+    roles_channel = await client.fetch_channel(ROLES_CHANNEL)
+    sent_message = await roles_channel.send(content=general_role_msg)
     for emoji in emojis:
-        print(emoji)
         await sent_message.add_reaction(emoji)
 
 @client.event
@@ -77,19 +92,21 @@ async def on_message(message: discord.Message):
         await send_init_message(message)
     elif message.author.id in admins and message.content.startswith("j!event"):
         # New event message
+        pick_role = get(message.guild.roles, name="Picking Competition")
         log_message("Started new picking competition event on {}".format(message.content[8:]))
-        sent_message = await message.channel.send("New picking competition on {}!\n\nIf you are interested in joining, use :judge: to be a judge and :student: to be a contestant.".format(message.content[8:]))
+        comp_channel = await client.fetch_channel(COMPETITION_CHANNEL)
+        sent_message = await comp_channel.send("{} New picking competition on **{}**!\n\nIf you are interested in joining, use :judge: to be a judge and :student: to be a contestant.".format(pick_role.mention, message.content[8:]))
         await sent_message.add_reaction("🧑‍⚖️")
         await sent_message.add_reaction("🧑‍🎓")
     elif message.content == "j!quote":
         # Quotes easter egg
         quote = random.choice(quotes)
         await message.channel.send(quote)
-        log_message("{} requested quote {} in {}".format(message.author.name + message.author.discriminator, quotes.index(quote), message.channel.name))
+        log_message("{} requested quote {} in {}".format(message.author.name + "#" + message.author.discriminator, quotes.index(quote), message.channel.name))
     elif message.content == "j!links":
         # Links to youtube/twitch
         await message.channel.send("Links:\nYoutube: <https://www.youtube.com/channel/UCzVdEwv0OYkXtoh5eOw0Tyw>\nTwitch: <https://www.twitch.tv/joir179>")
-        log_message("{} requested Joi links in {}".format(message.author.name + message.author.discriminator, message.channel.name))
+        log_message("{} requested Joi links in {}".format(message.author.name + "#" + message.author.discriminator, message.channel.name))
     
 
 @client.event
@@ -100,9 +117,9 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
     if user.id == BOT_ID:
         # Don't process bot actions
         return
-    if channel.name == ROLES_CHANNEL and payload.emoji.name in emote_to_role:
+    if channel.id == ROLES_CHANNEL and payload.emoji.name in emote_to_role:
         await user.add_roles(get(channel.guild.roles, name=emote_to_role[payload.emoji.name]))
-        log_message("Added {} role for {}".format(emote_to_role[payload.emoji.name], user.name + user.discriminator))
+        log_message("Added {} role for {}".format(emote_to_role[payload.emoji.name], user.name + "#" + user.discriminator))
 
 @client.event
 async def on_raw_reaction_remove(payload: discord.RawReactionActionEvent):
@@ -112,9 +129,9 @@ async def on_raw_reaction_remove(payload: discord.RawReactionActionEvent):
     if user.id == BOT_ID:
         # Don't process bot actions
         return
-    if channel.name == ROLES_CHANNEL and payload.emoji.name in emote_to_role:
+    if channel.id == ROLES_CHANNEL and payload.emoji.name in emote_to_role:
         await user.remove_roles(get(channel.guild.roles, name=emote_to_role[payload.emoji.name]))
-        log_message("Removed {} role from {}".format(emote_to_role[payload.emoji.name], user.name + user.discriminator))
+        log_message("Removed {} role from {}".format(emote_to_role[payload.emoji.name], user.name + "#" + user.discriminator))
 
 @client.event
 async def on_ready():
