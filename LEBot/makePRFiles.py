0b76900a877abf5b41c297b52215db01e2dc5197
Justin Reiter <JDReiter@uwaterloo.ca>
2021-07-24 19:25:24 -0400

    Update pr season init file

--- LEBot/makePRFiles.py
+++ LEBot/makePRFiles.py
@@ -1,5 +1,3 @@
-import os
-import discord
 import json
 
 
@@ -8,9 +6,11 @@ print("PR Setup by JustinR17\n")
 
 division_count = int(input("Number of divisions: "))
 
-divisions = ""
+divisions = {}
 for i in range(division_count):
-    divisions += input("Division name: ") + "\n"
-    divisions += input("Division id: ") + "\n"
-open("./games/tournamentIds", "w").write(divisions)
-open("./games/processedGames", "w").write("")
+    divisions[input("Division name: ")] = input("Division id: ")
+
+with open("./games/tournamentIds", "w") as writer:
+    json.dump(divisions, writer, sort_keys=True, indent=4)
+with open("./games/processedGames", "w") as writer:
+    json.dump([], writer, sort_keys=True, indent=4)
